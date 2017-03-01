172a12b3b08ad106b331f5fbcb37800138a3c0cf
James Fish <james@fishcakez.com>
2017-03-01 08:33:43 -0500

    Supervisors

---
+++ supervisors/problem_c/lib/problem_c.ex
@@ -0,0 +1,47 @@
+defmodule ProblemC do
+  @moduledoc """
+  ProblemC.
+  """
+
+  alias __MODULE__.Person
+
+  @doc """
+  Start two people: Alice and Bob, always and forever.
+  """
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    {:ok, _} = Person.start_link(:alice)
+    {:ok, _} = Person.start_link(:bob)
+    {:ok, nil}
+  end
+
+  ## Do not change code below
+
+  defdelegate learn(person, language), to: Person
+  defdelegate languages(person), to: Person
+  defdelegate forget(person), to: Person
+
+  defmodule Person do
+    @moduledoc false
+
+    def start_link(name) do
+      Agent.start_link(&MapSet.new/0, name: name)
+    end
+
+    def learn(person, language) do
+      Agent.update(person, &MapSet.put(&1, language))
+    end
+
+    def languages(person) do
+      Agent.get(person, &MapSet.to_list/1)
+    end
+
+    def forget(person) do
+      Agent.update(person, fn(_) -> MapSet.new() end)
+    end
+  end
+end
---
+++ supervisors/problem_i/lib/problem_i.ex
@@ -0,0 +1,67 @@
+defmodule ProblemI do
+  @moduledoc """
+  ProblemI.
+  """
+
+  alias __MODULE__.{Server, Client}
+
+  @doc """
+  Start the GenServers.
+  """
+  def start_link() do
+    max_restarts = 1
+
+    ## Do not change code below
+
+    import Supervisor.Spec, only: [worker: 2]
+
+    children = [worker(Server, []), worker(Client, [])]
+    opts = [strategy: :rest_for_one, max_restarts: max_restarts]
+    Supervisor.start_link(children, opts)
+  end
+
+  defmodule Server do
+    @moduledoc false
+
+    use GenServer
+    alias ProblemI.Client
+
+    def start_link() do
+      GenServer.start_link(__MODULE__, nil, [name: __MODULE__])
+    end
+
+    def call_me_back(), do: GenServer.cast(__MODULE__, {:call_me_back, self()})
+
+    def init(_) do
+      {:ok, nil}
+    end
+
+    def handle_cast({:call_me_back, pid}, _) do
+      {:noreply, Client.increment(pid)}
+    end
+  end
+
+  defmodule Client do
+    @moduledoc false
+
+    use GenServer
+    alias ProblemI.Server
+
+    def start_link() do
+      GenServer.start_link(__MODULE__, nil, [name: __MODULE__])
+    end
+
+    def increment(pid), do: GenServer.call(pid, :increment)
+
+    def init(_) do
+      Server.call_me_back()
+      {:ok, 0}
+    end
+
+    def handle_call(:increment, _, counter) do
+      Server.call_me_back()
+      counter = counter + 1
+      {:reply, counter, counter}
+    end
+  end
+end
